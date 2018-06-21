8370fb039a33cb93b04a481ebb9645289ca6dbff
Taddes Korris <taddes.korris@gmail.com>
2018-06-21 13:30:18 -0400

    added logic for new pipes

--- flappyBird.js
+++ flappyBird.js
@@ -1,3 +1,5 @@
+window.onload = function() {
+
 // Continually draw image to canvas
 const cvs = document.getElementById('canvas');
 
@@ -31,18 +33,49 @@ score.src = './sounds/score.mp3'
 let gap = 85;
 let constantValue = pipeNorth.height + gap;
 
+// Bird X and Y coordinates
 let birdX = 10;
 let birdY = 15
 
+// Gravity effect on bird
 let gravity = 1;
 
+// on Key event
+document.addEventListener('keydown', moveUp);
 
-draw = () => {
+function moveUp() {
+  birdY -= 20;
+}
+
+// Pipe Coordinates
+const pipe = [];
+
+pipe[0] = {
+  x: cvs.width,
+  y: 0
+}
+
+
+function draw() {
   // Background - center in canvas
   ctx.drawImage(bg, 0, 0);
+
   // Pipes - pipeSouth, constant added as pipe hight + gap
-  ctx.drawImage(pipeNorth, 100, 0);
-  ctx.drawImage(pipeSouth, 100, 0 + constantValue);
+  for( let i = 0; i > pipe.length; i++) {
+    ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
+    ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constantValue);
+
+    pipe[i].x--;
+
+    if(pipe[i].x == 125) {
+      pipe.push({
+        x: cvs.width,
+        y: Math.floor(Math.random() * pipeNorth.height - pipeNorth.hight)
+      });
+    }
+
+  }
+
 
   // Foreground - comes up from bottom, taking up canvas height - fg height
   // ctx.drawImage(fg, 0, cvs.height - fg.height);
@@ -58,3 +91,5 @@ draw = () => {
 
 // Invoke draw
 draw();
+
+}
