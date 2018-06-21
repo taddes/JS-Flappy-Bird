1a5a28d2f853f4cf20c2a5d29dd5a3e91a755e58
Taddes Korris <taddes.korris@gmail.com>
2018-06-21 13:06:43 -0400

    set up images and canvas

---
+++ flappyBird.js
@@ -0,0 +1,19 @@
+// Continually draw image to canvas
+const cvs = document.getElementById('canvas');
+
+// Creates 2d canvas and numerous methods to manipulate it
+const ctx = cvs.getContext('2d');
+
+// Images for game
+const bird = new Image();
+const bg = new Image();
+const fg = new Image();
+const pipeNorth = new Image();
+const pipeSouth = new Image();
+
+bird.src = 'images/bird.png';
+bg.src = 'images/bg.png';
+fg.src = 'images/fg.png';
+pipeNorth.src = 'images/pipeNorth.png';
+pipeSouth.src = 'images/pipeSouth.png';
+
